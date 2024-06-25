af316f128607ac4d11c381250b098b2a3b85fd58
someone2639 <someone2639@gmail.com>
2024-06-25 18:13:22 -0400

    add branch name too

--- MainPage_topbar.gd
+++ MainPage_topbar.gd
@@ -8,4 +8,10 @@ func _on_new_folder_button_pressed():
 	get_tree().change_scene_to_file("res://SelectDecomp.tscn")
 
 func _on_cur_path_tree_entered():
-	get_node("VBoxContainer/top_bar/HBoxContainer/cur_path").text = "Current Path: " + GlobalVars.file_path
+	get_node("VBoxContainer/top_bar/HBoxContainer/cur_path").text = "Current Repo: " + GlobalVars.file_path
+	if FileAccess.file_exists(GlobalVars.file_path+"/.git/HEAD"):
+		var file = FileAccess.open(GlobalVars.file_path+"/.git/HEAD", FileAccess.READ)
+		var buf = file.get_as_text()
+		file.close()
+		var branch = buf.split("/heads/")[1].strip_edges()
+		get_node("VBoxContainer/top_bar/HBoxContainer/cur_path").text += " (" + branch + ")"
